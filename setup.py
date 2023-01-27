6e08b9acba1086f42a6879a5421b4df3c056fbb9
Miquel Massot <miquel.massot@gmail.com>
2023-01-27 09:29:39 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ def main():
 
     skbuild.setup(
         name="g2o-python",
-        version="0.0.6",
+        version="0.0.7",
         url="https://github.com/miquelmassot/g2o-python",
         license="MIT",
         description="Wrapper package for G2O python bindings.",
